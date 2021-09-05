8a760e286a6b2ef5101fc921a6704c31040d4998
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-09-05 14:25:03 +0630

    [Section 02]07. Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
+    </article>
+    <a href="/">Go Back</a>
+    
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
+    </article>
+    
+</body>
