15d37e5e98f84e19c3c15c95ad2e8e109fbc28ce
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-09-12 22:08:26 +0630

    [Section 02]11. Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 
 <body>
-    <article>
+    {{-- <article>
         <h1><a href="/posts/my-first-post">My First Blog</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
     </article>
@@ -18,6 +18,10 @@
     <article>
         <h1><a href="/posts/my-third-post">My Third Blog</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
-    </article>
-    
+    </article> --}}
+    @foreach ( $posts as $post)
+        <article>
+            <?= $post; ?>
+        </article>
+    @endforeach
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,17 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+    return view('posts',[
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}',function($slug){
-    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html"))
-    {
-        abort(404);
-    }
 
-    $post = cache()->remember("posts.{slug}", 1200, fn() => file_get_contents($path));
-
-    return view('post',['post' => $post,]);
+    //find a post by its slug and pass it to a view called "post"
+    return view('post',[
+        'post' => Post::find($slug)
+    ]);
 
 })->where('post', '[A-z_\-]+');//regular expression; post = line no 28 'post'
