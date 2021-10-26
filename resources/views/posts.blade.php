995b6140f72010737f1cd8d8520d242e2dd022a5
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-10-27 00:24:03 +0630

    [Section 02]12.Find a Composer Package for Post Metadata

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -21,7 +21,12 @@
     </article> --}}
     @foreach ( $posts as $post)
         <article>
-            <?= $post; ?>
+            <a href="/posts/<?= $post->slug; ?>">
+                <h1><?= $post->title; ?></h1>
+            </a>
+            
+            <div><?= $post->excerpt; ?></div>
+            
         </article>
     @endforeach
 </body>
