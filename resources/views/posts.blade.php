a342251bec17cdb47fb0dcd7e6651ecc935f5cf2
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-10-27 23:43:16 +0630

    [Section 03]15.Blade Layouts Two Ways

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,24 +1,4 @@
-<!doctype html>
-
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-
-<body>
-    {{-- <article>
-        <h1><a href="/posts/my-first-post">My First Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, obcaecati quibusdam consectetur voluptate facilis unde quos ullam at? Officiis consectetur dolorum adipisci magni minima architecto rem tempore harum corporis tempora.</p>
-    </article> --}}
+<x-layout>
     @foreach ( $posts as $post)
         <article>
             <a href="/posts/{{ $post->slug }}">
@@ -29,4 +9,4 @@
             
         </article>
     @endforeach
-</body>
+</x-layout>
