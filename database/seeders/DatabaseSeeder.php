0881929f635c8ae5b255b693215b87cd91e60470
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-11-01 22:49:12 +0630

    [Section 04]28. Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,51 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $family =Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Mark',
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, tempore exercitationem, facere nisi unde perspiciatis consequuntur atque amet ex quod dolore quo asperiores sapiente omnis dolores nam! Cum, repellat qui.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, tempore exercitationem, facere nisi unde perspiciatis consequuntur atque amet ex quod dolore quo asperiores sapiente omnis dolores nam! Cum, repellat qui.</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My Hobbies Post',
-            'slug' => 'my-hobbies-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet consectetur</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, tempore exercitationem, facere nisi unde perspiciatis consequuntur atque amet ex quod dolore quo asperiores sapiente omnis dolores nam! Cum, repellat qui.</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
