3bd65ac4149343d857538835cf856546c44f8d75
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-11-05 00:26:33 +0630

    [Section 05]32. Convert the HTML and CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,22 @@
 <x-layout>
-    @foreach ( $posts as $post)
+    
+    @include('posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+      <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+           <x-post-card/>
+           <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+           <x-post-card/>
+           <x-post-card/>
+           <x-post-card/>            
+        </div>
+    </main>
+    {{-- @foreach ( $posts as $post)
         <article>
             <a href="/posts/{{ $post->slug }}">
                 <h1>{!! $post->title !!}</h1>
@@ -18,5 +35,5 @@
             <div>{!! $post->excerpt !!}</div>
             
         </article>
-    @endforeach
+    @endforeach --}}
 </x-layout>
