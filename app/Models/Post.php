447ccb10a28340dca19ee67e8a10bfc0cadc3471
Yoonmie <scm.yoonmiemieaung@gmail.com>
2021-11-09 00:22:23 +0630

    [Section 06]38. Search (The Cleaner Way)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,15 @@ class Post extends Model
 
    // protected $fillable=['title','excerpt','body'];
 
+   public function scopeFilter($query, array $filters)
+   {
+      //$search = $filters['search']
+      $query->when($filters['search'] ?? false, fn($query,$search) =>
+         $query
+            ->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%'));
+   }
+
    public function category()
    {
       return $this->belongsTo(Category::class);
